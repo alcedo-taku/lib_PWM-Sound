1fc5f92a45e9afc8d4e1cf998fa5a84ccd4ff4d2
立脇拓 <takuji.coroton.thanks@gmail.com>
2021-11-12 10:13:08 +0900

    refactor: prescaler で周波数を変更するようにした #3

--- PwmSounds.cpp
+++ PwmSounds.cpp
@@ -20,7 +20,8 @@ void PwmSounds::set_sounds(Music* sounds, uint8_t number_of_sound){
 void PwmSounds::start_sounds(){
 	if( is_playing == false ){ // もし停止状態だったら
 		// music start
-		HAL_TIM_PWM_Start(sound_htim, sound_channel);
+		__HAL_TIM_SET_AUTORELOAD( sound_htim, 99); // counter period を変更
+		HAL_TIM_PWM_Start( sound_htim, sound_channel );
 		end_time_of_sound = HAL_GetTick(); // music updateに進めるように、とりあえず代入
 		playing_sound = 0;
 		is_playing = true; // 再生中にする
@@ -34,8 +35,8 @@ bool PwmSounds::update_sounds(){
 			if(sounds[playing_sound].music_scale == 0){ // もし周波数が0(休符)だったら
 				__HAL_TIM_SET_COMPARE(sound_htim, sound_channel, 0); // デューティー比を0%にする
 			}else{
-				__HAL_TIM_SET_AUTORELOAD( sound_htim, 64000000/*HAL_RCC_GetHCLKFreq()*/ / (sound_htim->Init.Prescaler) / sounds[playing_sound].music_scale ); // counter period変更 → 周波数変更
-				__HAL_TIM_SET_COMPARE(sound_htim, sound_channel, (sound_htim->Init.Period)*0.5); // デューティー比を50%にする
+				__HAL_TIM_SET_PRESCALER( sound_htim, 64000000/*HAL_RCC_GetHCLKFreq()*/ / (sound_htim->Init.AutoReloadPreload) / sounds[playing_sound].music_scale ); // counter period変更 → 周波数変更
+				__HAL_TIM_SET_COMPARE( sound_htim, sound_channel, 50 ); // デューティー比を50%にする
 			}
 			end_time_of_sound = HAL_GetTick() + sounds[playing_sound].music_time; // soundTim 更新
 		}else{
